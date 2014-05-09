838b9243a8f033f5dd0b745d04920895b7ea5afa
KLX2287 <KIR27303@KIR27303-L11.ad.lpt.fi>
2014-05-09 14:39:08 +0300

    more files added and methods split up

---
+++ Interaction.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Speech;
+using Microsoft.Speech.Recognition;
+
+namespace SpeechRecognition
+{
+    public class Interaction
+    {
+        public Interaction() {}
+
+        public static void start()
+        {
+            // Create a SpeechRecognitionEngine object for the default recognizer in the en-US locale.
+            using (SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("en-US")))
+            {
+                // Configure the input to the speech recognizer.
+                recognizer.SetInputToDefaultAudioDevice();
+
+                Grammar interaction = Grammars.Speech();
+                interaction.Name = ("Speech analyser");
+                recognizer.LoadGrammarAsync(interaction);
+                // Add a handler for the speech recognized event.
+                recognizer.SpeechRecognized +=
+                  new EventHandler<SpeechRecognizedEventArgs>(Handler.recognizer_SpeechRecognized);
+                recognizer.SpeechHypothesized +=
+                  new EventHandler<SpeechHypothesizedEventArgs>(Handler.recognizer_SpeechHypothesized);
+                recognizer.SpeechRecognitionRejected +=
+                  new EventHandler<SpeechRecognitionRejectedEventArgs>(Handler.recognizer_SpeechRecognitionRejected);
+                recognizer.LoadGrammarCompleted +=
+                  new EventHandler<LoadGrammarCompletedEventArgs>(Handler.recognizer_LoadGrammarCompleted);
+                recognizer.AudioStateChanged +=
+                  new EventHandler<AudioStateChangedEventArgs>(Handler.recognizer_AudioStateChanged);
+
+                // Start asynchronous, continuous speech recognition.
+                recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+                // Keep the console window open.
+                while (true) Console.ReadLine();
+            }
+        }
+    }
+}
